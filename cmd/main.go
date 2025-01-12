f91362aeeb1a45d8e90d10ef640d063a16afb66d
takeshi.koenuma <t.koenuma2@gmail.com>
2025-01-12 18:53:37 +0900

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "slsa-test/pkg/hello"
+import (
+	"fmt"
+	"slsa-test/pkg/hello"
+)
+
+var gitTag = "dev"
 
 func main() {
 	hello.Hello()
+	fmt.Println("git tag: ", gitTag)
 }
